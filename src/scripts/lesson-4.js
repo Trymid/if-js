7e4781dc2409150492a50a1c270e658d48902d1e
Dmitry Grabovsky <grabovskydmitry@gmail.com>
2023-06-22 01:40:15 +0300

    refactor: add 'src' folder

---
+++ src/scripts/lesson-4.js
@@ -0,0 +1,39 @@
+export { sum, changeTextColor };
+
+function ordinarySum(x, y) {
+  return x + y;
+}
+
+function cur(f) {
+  return function (x) {
+    return function (y) {
+      return f(x, y);
+    };
+  };
+}
+const sum = cur(ordinarySum);
+
+const COLORS = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+const DATA = {};
+
+const BODY = document.getElementsByTagName('body');
+const PARAGRAPHS = document.querySelectorAll("[id *= 'text']");
+
+for (let i = 1; i <= PARAGRAPHS.length; i++) {
+  DATA['text' + i] = [-1, PARAGRAPHS[i - 1]];
+}
+
+function changeTextColor(event) {
+  const targetParagraph = event.target;
+
+  if (DATA[targetParagraph.id]) {
+    DATA[targetParagraph.id][0] += 1;
+    targetParagraph.style.color = COLORS[DATA[targetParagraph.id][0]];
+    if (DATA[targetParagraph.id][0] === COLORS.length - 1) {
+      DATA[targetParagraph.id][0] = -1;
+    }
+  }
+}
+BODY[0].addEventListener('click', (event) => {
+  changeTextColor(event);
+});
--- tests/lesson-4.test.js
+++ tests/lesson-4.test.js
@@ -1,15 +1,7 @@
-// import { randProductName, randNumber } from "@ngneat/falso";
-import {sum, addProduct} from "../scripts/lesson-4";
-
-// test('When adding new valid product, get successfull confirmation', () => {
-//   const name = randProductName();
-//   const price = randNumber();
-//   console.log(name, price)
-//
-//   expect(addProduct(name, price)).toBe(true);
-// });
-
+import { sum } from '../src/scripts/lesson-4';
 
 test("When launch function 'sum'", () => {
   expect(sum(5)(3)).toBe(8);
 });
+
+
